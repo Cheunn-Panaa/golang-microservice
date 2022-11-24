067603ccd8865b0f196a5e782ebb1c16255b58b0
Cheunn-Nourry <cheunn.nourry@steeple.com>
2022-11-24 23:04:18 +0100

    Adding Swagger docs

--- pkg/api/health.go
+++ pkg/api/health.go
@@ -1,22 +1,34 @@
-package health
+package api
 
 import (
 	"net/http"
-	"sync/atomic"
 )
 
-// Healthz is a liveness probe.
+// Healthz godoc
+// @Summary Liveness check
+// @Description used by Kubernetes liveness probe
+// @Tags Kubernetes
+// @Accept json
+// @Produce json
+// @Router /healthz [get]
+// @Success 200 {string} string "OK"
 func Healthz(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-// Readyz is a readiness probe.
-func Readyz(isReady *atomic.Value) http.HandlerFunc {
-	return func(w http.ResponseWriter, _ *http.Request) {
-		if isReady == nil || !isReady.Load().(bool) {
-			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-	}
+// Readyz godoc
+// @Summary Readiness check
+// @Description used by Kubernetes readiness probe
+// @Tags Kubernetes
+// @Accept json
+// @Produce json
+// @Router /readyz [get]
+// @Success 200 {string} string "OK"
+func Readyz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	//if atomic.LoadInt32(&ready) == 1 {
+	//	w.WriteHeader(http.StatusOK)
+	//	return
+	//}
+	//w.WriteHeader(http.StatusServiceUnavailable)
 }
